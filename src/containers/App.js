19a3adc4cbd410dd80d289f2ea012eeeaee33061
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-12-24 22:12:14 +0200

    Creating UIs

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import demoData from '../demoData/data';
+import CardSidePic from '../components/CardSidePic';
+import Container from '../components/Container';
+    return (
+      <div className="App">
+        <Container>
+          {this.state.list}
+        </Container>
+      </div>
+    )
+  }
+}
+
+export default App;
