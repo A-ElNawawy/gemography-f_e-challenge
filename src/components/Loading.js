e5f1532143590eab2cac12a9a99cfe067ec25485
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-01-03 17:57:16 +0200

    Generalize [ Loading ] Component

--- src/components/Loading.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Loading = styled.h2`
-  text-align: center;
-  margin-top: 20px;
-  text-transform: capitalize;
-  font-size: 30px;
-`;
-export default Loading;
---
+++ src/components/Message.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Message = styled.h2`
+  background-color: #000000a1;
+  color: #fff;
+  border: 2px solid #000;
+  border-radius: 5px;
+  text-transform: capitalize;
+  text-align: center;
+  font-size: 30px;
+
+  width: 95%;
+  padding: 30px 0;
+  margin: 0 20px;
+  position: fixed;
+  bottom: 50vh;
+  left: 0;
+`;
+export default Message;
