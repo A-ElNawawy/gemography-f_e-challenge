35f6c36b1dfdab495bcd2edf155bf4a92551eb58
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-01-03 14:24:59 +0200

    Retouch

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 const Loading = styled.h2`
   text-align: center;
-  margin-top: 200px;
+  margin-top: 20px;
   text-transform: capitalize;
-  font-size: 60px;
+  font-size: 30px;
 `;
 export default Loading;
