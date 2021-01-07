75ed7c3a8e85bb644cff648b4d6bd5eda3c1b8df
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2021-01-07 18:40:37 +0200

    Retouch

--- src/containers/App.js
+++ src/containers/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      coding: true, // just for testing [set it to prevent fetching data]
+      coding: false, // just for testing [set it to prevent fetching data]
       loading: false,
       items: [], // array of all repos we get from server
       message: "", // error message we get from server
